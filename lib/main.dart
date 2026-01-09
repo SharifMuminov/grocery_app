0dcad101061f0a8e46fd2d9be6151559c641cb5b
sharifboymuminov <muminovsharifboy@gmail.com>
2026-01-09 09:18:02 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/pages/on_boarding/on_boarding_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFFF9F9F9)),
+      home: const OnBoardingPage(),
+    );
+  }
+}
